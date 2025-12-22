ff0249d54e1d41ab59c993aa2b4d68d3a1e382ea
sunnycow <sannikov_yao@farpost.com>
2025-12-22 18:59:36 +1000

    Solve 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const convertedTemp = (temp - 32) * 5 / 9;
+
+  return parseFloat(convertedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+  const convertedTemp = temp * 9 / 5 + 32;
+
+  return parseFloat(convertedTemp.toFixed(1));
+ };
 
 // Do not edit below this line
 module.exports = {
