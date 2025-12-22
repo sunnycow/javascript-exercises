c32e0ddba2b5f11d77359aefd1e71198584c5b9e
sunnycow <sannikov_yao@farpost.com>
2025-12-22 16:37:33 +1000

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (values.includes(arr[i])) {
+      arr.splice(i, 1);
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
