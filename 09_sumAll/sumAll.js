877a911e7005e8c5f471001aff8a33d1a8b824e0
sunnycow <sannikov_yao@farpost.com>
2025-12-22 18:20:29 +1000

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if ((start < 0 || !Number.isInteger(start)) || (end < 0 || !Number.isInteger(end))) {
+    return 'ERROR';
+  }
 
+  // total count of numbers in the range
+  const count = Math.abs(start - end) + 1;
+
+  // use gauss summation formula
+  const sum = (count * (start + end)) / 2;
+
+  return sum;
 };
 
 // Do not edit below this line
