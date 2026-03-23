3283376ea553158231c63b23a9e7460568b1f012
sunnycow <sannikov_yao@farpost.com>
2026-03-23 10:59:57 +1000

    Solve 13_palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+/**
+ * @param {string} str 
+ */
 
+const palindromes = function (str) {
+  const cleaned = str.toLowerCase().replace(/[\W_]/g, '');
+
+  for (let i = 0; i < cleaned.length / 2; i++) {
+    if (cleaned[i] != cleaned[cleaned.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
